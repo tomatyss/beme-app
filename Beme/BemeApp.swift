9cdaf1389ee4f87a5124064a907ece5a23ca7ead
Ivan Iufriakov <tomatyss@gmail.com>
2023-02-26 17:51:56 -0500

    Initial Commit

---
+++ Beme/BemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BemeApp.swift
+//  Beme
+//
+//  Created by Ivan Yu on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
